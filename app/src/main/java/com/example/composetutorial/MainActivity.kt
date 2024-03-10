7e7f8c325280920dc1f1647c0b68e754df79cceb
Vishal Gupta <vishal.gupta@kellton.com>
2024-03-10 10:01:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composetutorial.ui.theme.HeadingText
+import com.example.composetutorial.ui.theme.LoginText
+import com.example.composetutorial.ui.theme.SubHeading
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            ShowText()
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun ShowText(){
+
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.White)
+        .padding(20.dp)
+    ){
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "backfround image",
+            colorFilter = ColorFilter.tint(color= Color.Red),
+            modifier = Modifier.background(color = Color.Black)
+        )
+
+        Spacer(modifier = Modifier.padding(5.dp))
+
+        Text(
+            text = "Getting Started",
+            style = MaterialTheme.typography.HeadingText
+        )
+
+        Spacer(modifier = Modifier.padding(5.dp))
+
+        Text(
+            text = "Let’s login for explore continues",
+            style = MaterialTheme.typography.SubHeading/*.copy(color= SubHeading)*/
+        )
+
+        Spacer(modifier = Modifier.padding(20.dp))
+
+        Text(
+            text = "Email or Phone Number",
+            style = MaterialTheme.typography.LoginText
+        )
+    }
+}
---
+++ app/src/main/java/com/example/composetutorial/ui/theme/Type.kt
@@ -0,0 +1,65 @@
+package com.example.composetutorial.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.composetutorial.R
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+        bodyLarge = TextStyle(
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                letterSpacing = 0.5.sp
+        )
+        /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
+
+val Typography.HeadingText: TextStyle
+        @Composable get() {
+                return TextStyle(
+                        color = Color.Black,
+                        fontSize = 24.sp,
+                        fontFamily = FontFamily(Font(R.font.roboto_regular, weight = FontWeight.Bold))
+                )
+        }
+
+val Typography.SubHeading: TextStyle
+        @Composable get() {
+                return TextStyle(
+                        fontSize = 12.sp,
+                        fontFamily = FontFamily(Font(R.font.roboto_regular, weight = FontWeight.Medium)),
+                        color = com.example.composetutorial.ui.theme.SubHeading
+                )
+        }
+
+val Typography.LoginText: TextStyle
+        @Composable get() {
+                return TextStyle(
+                        fontSize = 13.sp,
+                        fontFamily = FontFamily(Font(R.font.roboto_regular, weight = FontWeight.Medium)),
+                        color = Color.Black
+                )
+        }
